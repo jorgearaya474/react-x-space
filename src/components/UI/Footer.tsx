1380c13bd243ac66c3a9785c73baaf15bf1a1b8a
Jorgearaya47 <jorgearaya474@gmail.com>
2023-07-29 17:05:08 -0600

    project update

--- src/components/UI/Footer.tsx
+++ src/components/UI/Footer.tsx
@@ -2,11 +2,13 @@ import React, { FC } from 'react';
 
 const Footer: FC = () => {
     return (
-        <div className='flex justify-center items-center bg-black'>
-            <div className='container py-4'>
-                <p className='text-gray-500 text-sm font-normal text-center'>© 2023 Jorge Araya</p>
+        <footer>
+            <div className='flex justify-center items-center bg-black'>
+                <div className='container py-4'>
+                    <p className='text-gray-500 text-sm font-normal text-center'>© 2023 Jorge Araya</p>
+                </div>
             </div>
-        </div>
+        </footer>
     );
 }
 
--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -3,7 +3,7 @@ import React, { FC } from 'react';
 const Header: FC = () => {
     return (
         <header>
-            <div className='flex justify-center items-center bg-[url(/public/space-bk.jpg)] bg-no-repeat h-full bg-top'>
+            <div className='flex justify-center items-center bg-[url(/public/space-bk.jpg)] bg-no-repeat w-full h-full bg-top bg-cover'>
                 <div className='container py-28 px-4'>
                     <h1 className='text-white text-4xl font-bold text-center'>Space-X Past Launches 🚀</h1>
                     <p className='text-white text-xl font-medium text-center'>With GraphQL</p>
